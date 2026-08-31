4c0c7a2c2e35951463e745491dc4aa1f70cea3a1
sakic-a <xhb5q6wcvt@privaterelay.appleid.com>
2026-08-31 15:44:27 +0200

    fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['lucide-react', '@dnd-kit/core', '@dnd-kit/sortable', '@dnd-kit/utilities']
+        }
+      }
+    }
+  },
   server: {
     host: true,
     proxy: {
